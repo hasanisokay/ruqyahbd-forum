23b3ef45422d9de61221aaf4dd9faddfee2cf3f0
hasanisokay <hasanisokay@gmail.com>
2024-03-12 23:41:33 +0600

    stats and admins no chache

--- src/utils/getAdmins.js
+++ src/utils/getAdmins.js
@@ -1,10 +1,8 @@
-"use server"
-
 const getAdmins = async () => {
   try {
     const admin = await fetch(
-      `${process.env.NEXT_PUBLIC_BASEURL}/api/admin/chatdata`,
-      { cache: "no-store" }
+      `/api/admin/chatdata`,
+      { cache: 'no-cache' }
     );
     const data = await admin.json();
     return data;
--- src/utils/getStats.js
+++ src/utils/getStats.js
@@ -1,9 +1,8 @@
-"use server"
 const getStats = async () => {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASEURL}/api/getstat`,
-      { cache: "no-store" }
+      `/api/getstat`,
+      { cache: 'no-cache' }
     );
     const jsonData = await response.json();
     return jsonData;
